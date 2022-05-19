d750089b9b063468609f6f6efc6f07413dc2f397
Kristopher Kyle <kristopherkyle1@gmail.com>
2022-05-19 15:02:03 -0700

    update script

--- zzz_eval_scripts/bea_2022_eval_anon.py
+++ zzz_eval_scripts/bea_2022_eval_anon.py
@@ -591,6 +591,13 @@ def wbasic_stats(statsd,outname):
 	outf.flush()
 	outf.close()
 
+##########################################
+### Variables that need to be defined: ###
+##########################################
+goldpath = '/conllu_path/' #UPDATE THIS: path to folder with gold annotated .conllu files
+spacy_path = '/spacy_models_path/' #UPDATE THIS: path to folder with spacy models
+report_path = '/path_for_report_folder/' #UPDATE THIS: Path to where reports should be written
+
 ####################
 ### POS Analyses ###
 ####################
@@ -600,7 +607,6 @@ target_tags_fs = ['NN', 'IN', 'DT', 'PRP', 'JJ', 'RB', 'NNP', 'VB', 'NNS', 'CC',
 
 
 ### Target Gold Corpora ###
-goldpath = '/Users/kristopherkyle/Desktop/Corpora/Accuracy Project/'
 esl_wr_gold_dev = pos_reader_conllu(glob.glob(goldpath + "ESL_Written/*dev.conllu")) #500
 esl_sp_gold_dev = pos_reader_conllu(glob.glob(goldpath + "L2*/*dev.conllu")) #741
 ud_ewt_gold_dev = pos_reader_conllu(glob.glob(goldpath + "UD_English-EWT/*dev.conllu")) #1998
@@ -611,8 +617,6 @@ esl_sp_gold_test = pos_reader_conllu(glob.glob(goldpath + "L2*/*test.conllu")) #
 ud_ewt_gold_test = pos_reader_conllu(glob.glob(goldpath + "UD_English-EWT/*test.conllu")) #2076
 ud_gum_gold_test = pos_reader_conllu(glob.glob(goldpath + "*/en_gum-ud-test.conllu")) #1990; GUM + GUM Reddit
 
-#POS models:
-spacy_path = '/Users/kristopherkyle/Desktop/Programming/SpacyModels/'
 #trained on POS sets:
 
 #Trained with all SL2T POS data (as applicable):
@@ -625,16 +629,15 @@ gc_list = [(esl_sp_gold_dev,"L2 Spoken Dev"),(esl_wr_gold_dev,"L2 Written Dev"),
 tgr_listsp_small_pos = [(spacy_tag,l1comb_trf_small,"Spacy_trf UD"),(spacy_tag,l1l2comb_trf_small_pos,"Spacy_trf UD + L2"),(spacy_tag,l1l2ecomb_trf_small_pos,"Spacy_trf UD + L2 Equal")]
 
 spacy_tagger_results_20220330 = pos_compile_full(gc_list,tgr_listsp_small_pos)
-pickle.dump(spacy_tagger_results_20220330,open('/Volumes/GoogleDrive/My Drive/2019_20_LL_POS/Datasets/annotators/2021 zzz Final/20220330_bea/spacy_small_results_20220330.pickle',"wb"))
+pickle.dump(spacy_tagger_results_20220330,open(report_path + 'spacy_small_results.pickle',"wb"))
 spacy_report_20220330 = pos_table_maker_summary(spacy_tagger_results_20220330,target_tags_fs)
-wpos_report(spacy_report_20220330,"/Volumes/GoogleDrive/My Drive/2019_20_LL_POS/Datasets/annotators/2021 zzz Final/20220330_bea/spacy_small_pos_summary_20220330_test.tsv")
+wpos_report(spacy_report_20220330,report_path + "pos_summary.tsv")
 
 ### Dependency Analyses ###
 
 #target dependency tags:
 tdeps = ["case","nsubj","det","root","advmod","amod","obj","obl","nmod","mark","conj","cc","compound","aux","cop","nmod:poss","advcl","xcomp","acl:relcl","ccomp","nummod","flat","acl","aux:pass","parataxis","appos","nsubj:pass","discourse","compound:prt","expl","fixed","dep","obl:tmod","obl:npmod","nmod:tmod","iobj","csubj","list","det:predet","reparandum","goeswith","nmod:npmod","cc:preconj","vocative","obl:agent","orphan","dislocated","csubj:pass","flat:foreign"]
 
-goldpath = '/Users/kristopherkyle/Desktop/Corpora/Accuracy Project/'
 esl_wr_gold_dep_test = conllu_extract_dep_full(glob.glob(goldpath + "ESL_Written/*test.conllu")) #500
 esl_sp_gold_dep_test = conllu_extract_dep_full(glob.glob(goldpath + "L2*_dep/*test.conllu")) #232
 ud_ewt_gold_dep_test = conllu_extract_dep_full(glob.glob(goldpath + "UD_English-EWT/*test.conllu")) #2001
@@ -649,16 +652,16 @@ prs_list = [(spacy_parse_cpos,l1comb_trf,"Spacy_L1_UD_TRF"),(spacy_parse_cpos,l1
 gdc_list = [(esl_sp_gold_dep_test,"L2 Spoken"),(esl_wr_gold_dep_test,"L2 Written"),(ud_ewt_gold_dep_test,"EWT"),(ud_gum_gold_dep_test,"GUM")]
 
 dep_comp_test = dep_compile_full(gdc_list,prs_list)
-pickle.dump(dep_comp_test,open('/Volumes/GoogleDrive/My Drive/2019_20_LL_POS/Datasets/annotators/2021 zzz Final/20220330_bea/spacy_dep_results_20220330.pickle',"wb"))
+pickle.dump(dep_comp_test,open(report_path + 'spacy_dep_results.pickle',"wb"))
 dep_las_tbl = dep_table_maker_summary(dep_comp_test,tdeps,acctype = "LAS")
 # dep_ulas_tbl = dep_table_maker_summary(dep_comp,tdeps,acctype = "ULAS")
 # dep_ls_tbl = dep_table_maker_summary(dep_comp,tdeps,acctype = "LS")
 
 
-report_path = "/Volumes/GoogleDrive/My Drive/2019_20_LL_POS/Datasets/annotators/2021 zzz Final/20220330_bea/"
-wpos_report(dep_las_tbl, report_path + "dep_las_summary_f1_20220330_test.tsv")
-# wpos_report(dep_ulas_tbl, report_path + "dep_ulas_summary_f1_20220315.tsv")
-# wpos_report(dep_ls_tbl, report_path + "dep_ls_summary_f1_20220315.tsv")
+
+wpos_report(dep_las_tbl, report_path + "dep_las_summary_f1.tsv")
+# wpos_report(dep_ulas_tbl, report_path + "dep_ulas_summary_f1.tsv")
+# wpos_report(dep_ls_tbl, report_path + "dep_ls_summary_f1.tsv")
 
 ### Get basic stats:
 
